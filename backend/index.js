8f46de80fc849af9de9bb98c760b1f8b0585d1b2
peehultina23 <peehulpradhan23bsd027@gmail.com>
2025-08-17 22:05:00 +0530

    create sigup api route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config()
 mongoose.connect(process.env.MONGO_URI).then(
@@ -14,6 +15,9 @@ mongoose.connect(process.env.MONGO_URI).then(
 const app = express();
 const PORT = 3000;
 
+//for allowing json object in req body
+app.use(express.json())
+
 app.get("/", (req, res) => {
   res.send("🚀 Backend server is running!");
 });
@@ -21,3 +25,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+ 
+app.use("/api/auth", authRoutes)
