0baa35d87b0bbecf0af915ea1e9360c672b1c931
peehultina23 <peehulpradhan23bsd027@gmail.com>
2025-08-17 18:40:38 +0530

    React-router-dom installation and setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
 const App = () => {
-  return <div class="text-3xl bg-red-400">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/sign-up" exact element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
